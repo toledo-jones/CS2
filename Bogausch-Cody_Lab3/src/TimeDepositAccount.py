7e348bc04a0c19001a05332e1589bb41ccc63aef
toledo-jones <cody.peter.bogausch@gmail.com>
2024-02-08 15:10:13 -0500

    Finished Lab 2

--- Bogausch-Cody_Lab3/src/TimeDepositAccount.py
+++ Bogausch-Cody_Lab3/src/TimeDepositAccount.py
@@ -11,6 +11,7 @@
       setTimeBalance: set a new # of months left until a penalty free withdraw
       getTimeBalance: returns current number of months left until a penalty free withdraw
       addInterest: adds interest based on the rate and current account balance
+      withdraw: overrides parent withdraw to impose restrictions on when a deposit can be made
    Constants:
       PENALTY_FEE: cost to be added onto a withdraw when it is made before the time balance is 0
                     (penalize the user for early withdrawl)
@@ -32,28 +33,10 @@ class TimeDepositAccount(SavingsAccount):
     #    initialBalance: (float) starting balance of the account
     #    monthNumber: (int) number of months left until the deposit can be withdrawn without penalty
     # OUTPUT
-    #    ???
+    #    None
     def __init__(self, rate, initialBalance, monthNumber):
         super().__init__(rate, initialBalance)
-        self.setTimeBalance(monthNumber)
-
-    # Description:
-    #    Set Time Balance
-    # INPUT:
-    #    monthNumber: (int) number of months left until the deposit can be withdrawn without penalty
-    # OUTPUT
-    #    None
-    def setTimeBalance(self, monthNumber):
         self.timeBalance = monthNumber
-
-    # Description:
-    #    Get Time Balance
-    # INPUT:
-    #    None
-    # OUTPUT
-    #    timeBalance: (int) number of months remaining until the deposit can be withdrawn without penalty
-    def getTimeBalance(self):
-        return self.timeBalance
 
     # Description:
     #    Adds interest and ticks down the time balance by one month.
@@ -67,8 +50,8 @@ class TimeDepositAccount(SavingsAccount):
         super().addInterest()
 
         # Tick down the time balance by one month
-        self.setTimeBalance(self.getTimeBalance() - 1)
-
+        self.timeBalance -= 1
+    
     # Description:
     #    Overrides parent withdraw method to impose restrictions on the user for how long they must wait before
     #    making a withdraw
@@ -82,11 +65,14 @@ class TimeDepositAccount(SavingsAccount):
         # Check to see if the withdraw will incur a penalty
         if self.timeBalance > 0:
             print(f"Fee of ${self.PENALTY_FEE} is added withdraw amount of {amount}")
+            
             # The withdraw is penalized by adding the penalty fee to the withdraw amount
             amount += self.PENALTY_FEE
+        
         # Check to see if the withdraw goes through. The account may not have enough funds.
         if not super().withdraw(amount):
             return False
+        
         # Send back to the penalty fee in a return value to so that is
         # removed from the account but not given to the customer.
         # This will require some additional code in the ATM. Or the bank teller. Or whoever is using this code.
