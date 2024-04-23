e7628e631d6779c212f58215e1527dfd7bdc99a7
toledo-jones <cody.peter.bogausch@gmail.com>
2024-04-23 16:17:54 -0400

    Progressed on Lab 12

---
+++ Labs/Bogausch-Cody_Lab12/Dummy_Structure.c
@@ -0,0 +1,31 @@
+/*
+Create a small C program that contains: a dummy structure called blank (a.k.a. a structure without any variables/values),
+a dummy function (a.k.a. a function with two parameters, but no body).
+The program will also contain a main function (this will be of type int, and will return 0).
+
+The main function will have the following:
+   a) Definitions for: an integer, a single character, and a structure definition of type blank.
+   b) Pointer definitions for the integer, the character, the structure, for the dummy function, and a void pointer.
+   c) Declare an array with three integers.
+   d) Declare a new pointer variable called ptr (but do not assign it to anything).
+   e) Assign the address of val[0] to ptr.
+   f) Declare another pointer, called ptr2.
+   g) Assign ptr to ptr2.
+   h) Using the unary operator—the sizeof function, and printf, create a block of print statements. Each one will print out the size of a pointer you have created.
+   i) Traverse the array using a for loop that goes from 0 to 5, and a printf statement that prints the location of: *ptr++.
+What is going on in this loop?
+What’s correct about this code?
+Is there anything wrong with this code, and how can it be fixed?
+   Include your answers in a printf statement block.
+
+*/
+
+#include <stdio.h>
+int main(){
+    return 0;
+}
+
+//
+void funct(int a, int b) {}
+
+struct blank {}
